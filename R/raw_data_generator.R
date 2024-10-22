b8d76e351a46700fb8b8d8ffb79996a5192e8623
Roman Rogoza <rogozaroman.rr@gmail.com>
2024-10-22 19:57:20 +0100

    bugfixes

--- R/raw_data_generator.R
+++ R/raw_data_generator.R
@@ -65,7 +65,7 @@ raw_data_generator <- function(
     # Generate raw data for each study configuration in the template
     raw_data_list <- lapply(seq_len(nrow(template)), function(i) {
       curr_vars <- template[i, ]
-      generate_rawdata_for_single_snapshot(
+      generate_rawdata_for_single_study(
         SnapshotCount = curr_vars$SnapshotCount,
         ParticipantCount = curr_vars$ParticipantCount,
         SiteCount = curr_vars$SiteCount,
@@ -79,7 +79,7 @@ raw_data_generator <- function(
 
   } else {
     # Generate raw data for the single study configuration provided
-    raw_data_list[[StudyID]] <- generate_rawdata_for_single_snapshot(
+    raw_data_list[[StudyID]] <- generate_rawdata_for_single_study(
       SnapshotCount = SnapshotCount,
       ParticipantCount = ParticipantCount,
       SiteCount = SiteCount,
