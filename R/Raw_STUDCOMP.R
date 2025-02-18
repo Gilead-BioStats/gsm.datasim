ff1bc4a58d6c34d03d68158c639017deb397134e
zdz2101 <zelos.zhu@atorusresearch.com>
2025-02-18 13:05:34 -0800

    temp solution for additional studycomp components

--- R/Raw_STUDCOMP.R
+++ R/Raw_STUDCOMP.R
@@ -25,10 +25,45 @@ Raw_STUDCOMP <- function(data, previous_data, spec, ...) {
   return(res)
 }
 
+Raw_StudyCompletion <- function(data, previous_data, spec, ...) {
+  # Function body for Raw_StudyCompletion
+  inps <- list(...)
+
+  curr_spec <- spec$Raw_StudyCompletion
+
+  if ("Raw_StudyCompletion" %in% names(previous_data)) {
+    dataset <- previous_data$Raw_StudyCompletion
+    previous_row_num <- nrow(dataset)
+  } else {
+    dataset <- NULL
+    previous_row_num <- 0
+  }
+
+  n <- inps$n - previous_row_num
+  if (n == 0) return(dataset)
+
+  args <- list(
+    subjid = list(n, data$Raw_SUBJ$subjid, replace = FALSE),
+    default = list(n)
+  )
+
+  res <- add_new_var_data(dataset, curr_spec, args, spec$Raw_StudyCompletion, ...)
+
+  return(res)
+}
+
 compyn <- function(n, ...) {
   # Function body for compyn
   sample(c("", "N"),
          prob = c(0.1, 0.9),
          n,
          replace = TRUE)
+}
+
+completion_reason <- function(n, ...) {
+  rep("TERMINATION OF MEDICATION", n)
+}
+
+completion_date <- function(n, ...) {
+  rep(as.Date(Sys.Date()), n)
 }
