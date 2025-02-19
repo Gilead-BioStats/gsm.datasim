8d44548954d2ee8c364053eb6afd7680907de206
zdz2101 <zelos.zhu@atorusresearch.com>
2025-02-19 10:22:29 -0800

    modify for more useful example

--- inst/examples/check_iterative_run.R
+++ inst/examples/check_iterative_run.R
@@ -1,5 +1,18 @@
 devtools::load_all()
 library(dplyr)
 
-result <- raw_data_generator(template_path = "~/gsm.datasim/data-raw/template.csv")
+single_result <- generate_rawdata_for_single_study(SnapshotCount = 3,
+                                                   ParticipantCount = 50,
+                                                   SiteCount = 5,
+                                                   StudyID = "ABC",
+                                                   workflow_path = "workflows/1_mappings",
+                                                   mappings = c("AntiCancer", "Baseline", "Consents", "Death", "OverallResponse", "StudyCompletion"),
+                                                   package = "gsm.endpoints",
+                                                   desired_specs = NULL)
 
+result <- raw_data_generator(template_path = "~/gsm.datasim/data-raw/template.csv",
+                             workflow_path = "workflows/1_mappings",
+                             mappings = c("AntiCancer", "Baseline", "Consents", "Death", "OverallResponse", "StudyCompletion"),
+                             package = "gsm.endpoints")
+
+result2 <- raw_data_generator(template_path = "~/gsm.datasim/data-raw/small_template.csv")
