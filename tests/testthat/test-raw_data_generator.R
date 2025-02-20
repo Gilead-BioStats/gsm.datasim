cbce33dabe64eb2600b2a626e16396bf2710fb52
zdz2101 <zelos.zhu@atorusresearch.com>
2025-02-20 10:40:11 -0800

    does that fix it?

--- tests/testthat/test-raw_data_generator.R
+++ tests/testthat/test-raw_data_generator.R
@@ -1,5 +1,5 @@
 test_that("generate_rawdata_for_single_study works", {
-  result <- raw_data_generator(template_path = "inst/small_template.csv")
+  result <- raw_data_generator(template_path = system.file("small_template.csv", package = "gsm.datasim"))
 
   expect_equal(length(result), 3)
   expect_equal(names(result), c("S0001", "S0004", "S0006"))
