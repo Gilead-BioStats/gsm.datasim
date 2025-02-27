64c424a83b200e6add8cb21239320392bdd3d694
zdz2101 <zelos.zhu@atorusresearch.com>
2025-02-27 13:01:12 -0800

    do specific mappings

--- tests/testthat/test-raw_data_generator.R
+++ tests/testthat/test-raw_data_generator.R
@@ -1,5 +1,8 @@
 test_that("generate_rawdata_for_single_study works", {
-  result <- raw_data_generator(template_path = system.file("small_template.csv", package = "gsm.datasim"))
+  result <- raw_data_generator(
+    template_path = system.file("small_template.csv", package = "gsm.datasim"),
+    mappings = c("AE")
+  )
 
   expect_equal(length(result), 3)
   expect_equal(names(result), c("S0001", "S0004", "S0006"))
