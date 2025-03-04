c13e580b44c5fe0246bbdbc0447e547fe3ebe2e1
zdz2101 <zelos.zhu@atorusresearch.com>
2025-03-04 13:41:18 -0800

    import a shim

--- R/gsm.datasim-package.R
+++ R/gsm.datasim-package.R
@@ -2,12 +2,12 @@
 "_PACKAGE"
 
 #' @import dplyr
+#' @import purrr
 #' @importFrom arrow write_parquet
 #' @importFrom glue glue
 #' @importFrom gsm MakeWorkflowList RunWorkflows
 #' @importFrom gsm.mapping CombineSpecs
 #' @importFrom logger log_info
-#' @importFrom purrr map imap
 #' @importFrom rlang zap
 #' @importFrom tictoc tic toc
 #' @importFrom utils read.csv
